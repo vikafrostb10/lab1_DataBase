a8020da013f9062ba21756da4e33b93061f94893
vikafrostb10 <58080864+vikafrostb10@users.noreply.github.com>
2021-03-25 22:18:11 +0200

    Update Lab3.sql

--- Lab3.sql
+++ Lab3.sql
@@ -15,9 +15,45 @@ CREATE VIEW super_vip_clients AS
 SELECT Payment.customerId, Payment.discount  FROM Payment
 WHERE discount = 15;
 
---4. 
+--4. Union of 2 tables
 SELECT price FROM orderDetail
 UNION
 SELECT price FROM Product
 ORDER BY price;
 
+--5. Find customers with the hightest discount
+SELECT customerId, MAX(discount) FROM Payment;
+
+--6. Merge name from Customer table with discount less than 5% grouped by Customer Id
+SELECT customerId, name FROM Customer WHERE
+name in (SELECT customerId FROM Payment WHERE discount < 5)
+GROUP BY customerId;
+
+--7 selects all orders with customer and price of each complained order information
+SELECT orderr.orderId, customer.name, Payment.price
+FROM ((orderr
+INNER JOIN Customers ON orderr.customerId = customer.customerId)
+INNER JOIN Payment ON orderr.orderId = Payment.customerId);
+
+--8 lists the Product Name if it finds ANY records in the orderDetail table has amount more than 4 
+SELECT name
+FROM Product
+WHERE productID = ANY
+  (SELECT productId
+  FROM orderDetail
+  WHERE amount > 4);
+  
+--9 lists the customers' names with a total price for oder more than 5000 UAH
+SELECT name
+FROM Customer
+WHERE EXISTS (SELECT orderId FROM orderr WHERE Customer.customerId = orderr.customerId AND totalPrice > 5000);
+        
+--10  lists the name of customers, sorted high to low customer ID (Only include first 100 customers)
+SELECT COUNT(customerID), name
+FROM Customer
+GROUP BY name
+HAVING COUNT(customerID) < 100
+ORDER BY COUNT(customerID) DESC;
+
+
+  
